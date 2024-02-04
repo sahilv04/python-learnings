1e980a54d51b6e3662fc5fd36088e27a7e234e69
Sahil <sahilv04@gmail.com>
2024-02-04 20:55:24 +0000

    variables

--- basics.py
+++
@@ -1,5 +0,0 @@
-n=1
-s="this is a string"
-
-print(n,s)
-print(type(n), type(s))
---
+++ variables.py
@@ -0,0 +1,14 @@
+n=1
+s="this is a string"
+name="Sahil"
+age=29
+exact_age=29.3333
+
+print(n,s)                      ## Print
+print(type(n), type(s))         ## Print types
+
+print("Hello this is %s"% name)
+print("%d is my age"% age)
+print("To be pricise, age is %f"% exact_age)
+print("With 2 digits after dot %.2f"% exact_age)
+print("Hex representations in upper and lower case:- %x and %X"% (age, age))
